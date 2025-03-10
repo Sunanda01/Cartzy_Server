de46886500893e0136e49397e7265a8bf4e7c31e
Sunanda01 <sunandasadhukhan1@gmail.com>
2025-03-10 20:41:18 +0530

    Created AuthController for register and login

---
+++ Controller/auth/auth-controller.js
@@ -0,0 +1,69 @@
+const User = require("../../Models/User");
+const jwt = require("jsonwebtoken");
+const bcrypt=require('bcryptjs');
+const SaltLevel = require('../../Config/config').SALT_LEVEL;
+const JWTHASHVALUE = require("../../Config/config").JWTHASHVALUE;
+const JWTTOKENEXPIRY = require("../../Config/config").JWTTOKENEXPIRY;
+
+const registerUser = async (req, res) => {
+  const { userName, email, password } = req.body;
+  try {
+    existUser = await User.findOne({ email });
+    if (existUser) {
+      return res.json({ success: false, msg: "User Already Exists" });
+    }
+    const Salt=await bcrypt.genSalt(Number(SaltLevel));
+    const hashPassword = await bcrypt.hash(password, Salt);
+    const newUser = new User({
+    userName,
+      email,
+      password: hashPassword,
+    });
+    await newUser.save();
+    res
+      .status(200)
+      .json({ success: true, msg: "User Registered Successfully" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false, msg: "Some Error Occured" });
+  }
+};
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const checkUser = await User.findOne({ email });
+    if (!checkUser) {
+      return res.json({ success: false, msg: "User Not Found" });
+    }
+    const checkPassword = await bcrypt.compare(password, checkUser.password);
+    if (!checkPassword) {
+      return res.json({ success: false, msg: "Incorrect password!!!!" });
+    }
+    const token = jwt.sign(
+      {
+        id: checkUser._id,
+        userName: checkUser.userName,
+        email: checkUser.email,
+        role: checkUser.role,
+      },
+      JWTHASHVALUE,
+      { expiresIn: JWTTOKENEXPIRY }
+    );
+    res.json({
+      success: true,
+      msg: "Logged In Successfully",
+      user: {
+        id: checkUser._id,
+        userName: checkUser.userName,
+        email: checkUser.email,
+        role: checkUser.role,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false, msg: "Some Error Occured" });
+  }
+};
+
+module.exports = { registerUser, loginUser };
--- index.js
+++ index.js
@@ -1,27 +1,35 @@
-const express=require('express');
-const PORT=require('./Config/config').PORT;
-const connection=require('./Utils/connection');
-const cors=require('cors');
-const cookieParser = require('cookie-parser');
-const FRONTEND_URL=require('./Config/config').FRONTEND_URL;
+const express = require("express");
+const PORT = require("./Config/config").PORT;
+const connection = require("./Utils/connection");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const FRONTEND_URL = require("./Config/config").FRONTEND_URL;
+const authRoutes = require("./Routes/auth/authRoutes");
+const app = express();
 
-const app=express();
 app.use(express.json());
-app.use(cors({
-    origin:FRONTEND_URL,
-    methods:['GET','POST','PUT','DELETE',],
-    allowedHeaders:[
-        'Content-Type',
-        'Authorization',
-        'Cache-Control',
-        'Expires',
-        'Pragma'
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
     ],
-    credentials:true
-}));
-app.use(express.urlencoded({extended:true}));
+    credentials: true,
+  })
+);
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.listen(PORT,()=>{
-    connection();
-    console.log(`PORT Connected @ ${PORT}`);
-})
+
+//Auth Routes
+app.use("/api/auth", authRoutes);
+
+
+app.listen(PORT, () => {
+  connection();
+  console.log(`PORT Connected @ ${PORT}`);
+});
