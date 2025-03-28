2c0aa0437b84f068f14a05df36141d0389804e7b
Sunanda01 <sunandasadhukhan1@gmail.com>
2025-03-28 18:30:43 +0530

    Product Controller

---
+++ Controller/admin/productController.js
@@ -0,0 +1,108 @@
+const { imageUploadUtil } = require("../../Middleware/cloudinary");
+const Product = require("../../Models/Product");
+
+const handleImageUpload = async (req, res) => {
+  try {
+    const b64 = Buffer.from(req.file.buffer).toString("base64");
+    const url = "data:" + req.file.mimetype + ";base64," + b64;
+    const result = await imageUploadUtil(url);
+    res.json({ success: true, result });
+  } catch (err) {
+    console.error(err);
+    res.json({ success: false, msg: "Error Occurred" });
+  }
+};
+
+const addProduct = async (req, res) => {
+  try {
+    const {
+      image,
+      title,
+      description,
+      category,
+      brand,
+      price,
+      salePrice,
+      totalStock,
+    } = req.body;
+    const newlyCreatedProduct = new Product({
+      image,
+      title,
+      description,
+      category,
+      brand,
+      price,
+      salePrice,
+      totalStock,
+    });
+    await newlyCreatedProduct.save();
+    res.status(201).jsopn({success:true,msg:"Product Added",data:newlyCreatedProduct});
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false, msg: "Failed to Add Product" });
+  }
+};
+
+const fetchAllProduct = async (req, res) => {
+  try {
+    const listOfProduct=await Product.find({});
+    res.status(200).json({success:true,data:listOfProduct});
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false, msg: "Failed to Fetch Product" });
+  }
+};
+
+const editProduct = async (req, res) => {
+  try {
+    const {id}=req.params;
+    const {
+        image,
+        title,
+        description,
+        category,
+        brand,
+        price,
+        salePrice,
+        totalStock,
+      } = req.body;
+      const findProduct=await Product.findById(id);
+      if(!findProduct){
+        return res.status(404).json({success:false,msg:"Product Not Found"});
+      }
+      findProduct.title= title || findProduct.title;
+      findProduct.description= description || findProduct.description;
+      findProduct.category= category || findProduct.category;
+      findProduct.brand= brand || findProduct.brand;
+      findProduct.price= price || findProduct.price;
+      findProduct.salePrice= salePrice || findProduct.salePrice;
+      findProduct.totalStock= totalStock || findProduct.totalStock;
+      findProduct.image= image || findProduct.image;
+      await findProduct.save();
+      res.status(200).json({success:true,msg:"Product Updated Successfully"})
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false, msg: "Failed to Edit Product" });
+  }
+};
+
+const deleteProduct = async (req, res) => {
+  try {
+    const {id} = req.params;
+    const product=await Product.findByIdAndDelete(id);
+    if(!product) return res.status(404).json({success:false,msg:"Product Not Found"});
+    res.status(200).json({success:true,msg:"Product Deleted Successfully"});    
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false, msg: "Failed to Delete Product" });
+  }
+};
+
+module.exports = {
+  handleImageUpload,
+  addProduct,
+  editProduct,
+  deleteProduct,
+  fetchAllProduct,
+};
--- Routes/admin/productRoutes.js
+++ Routes/admin/productRoutes.js
@@ -1,5 +1,9 @@
-const {handleImageUpload} = require('../../Controller/productController/handleImage');
+const {handleImageUpload, addProduct, editProduct, fetchAllProduct, deleteProduct} = require('../../Controller/admin/productController');
 const { upload } = require('../../Middleware/cloudinary');
 const router=require('express').Router();
 router.post('/upload-image',upload.single('my_file'),handleImageUpload);
+router.post('/add',addProduct);
+router.post('/edit/:id',editProduct);
+router.post('/get',fetchAllProduct);
+router.post('/delete/:id',deleteProduct);
 module.exports=router;
