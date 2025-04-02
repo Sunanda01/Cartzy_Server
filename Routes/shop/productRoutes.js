aa215e8f7a879945ea1e53c90570106a60936a03
Sunanda01 <sunandasadhukhan1@gmail.com>
2025-04-02 14:00:13 +0530

    Added new Product Controller and Route for it

--- Routes/shop/productRoutes.js
+++ Routes/shop/productRoutes.js
@@ -1,7 +1,9 @@
 const {
   getFilteredProduct,
+  getProductDetails,
 } = require("../../Controller/shop/productsController");
 
 const routes = require("express").Router();
 routes.get("/get", getFilteredProduct);
+routes.get("/get/:id",getProductDetails);
 module.exports = routes;
