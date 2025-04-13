62ad24e2ca1e06654a8285c469c5b77e4580d69d
Sunanda01 <sunandasadhukhan1@gmail.com>
2025-04-13 16:09:39 +0530

    fix

--- Routes/admin/orderRoutes.js
+++ Routes/admin/orderRoutes.js
@@ -3,10 +3,10 @@ const {
   getOrderDetails,
   updateOrderStatus,
 } = require("../../Controller/admin/orderController");
-const { verifyToken, verifyAdmin } = require("../../Middleware/verification");
+// const { verifyToken, verifyAdmin } = require("../../Middleware/verification");
 
 const routes = require("express").Router();
 routes.get("/get-order", getAllOrder);
-routes.get("/get-order-details/:id", verifyToken, verifyAdmin, getOrderDetails);
-routes.put("/update/:id", verifyToken, verifyAdmin, updateOrderStatus);
+routes.get("/get-order-details/:id",  getOrderDetails);
+routes.put("/update/:id", updateOrderStatus);
 module.exports = routes;
