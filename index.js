b5e084850ccb4444136003bffd4fa5373966063e
Sunanda01 <sunandasadhukhan1@gmail.com>
2025-04-13 15:40:44 +0530

    fix

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: FRONTEND_URL,
+    origin: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: [
       "Content-Type",
